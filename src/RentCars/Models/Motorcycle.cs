208102e4a798fd8e6127c0f141f85e89781891e6
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-07-11 14:47:53 -0300

    requisito 7

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -6,6 +6,6 @@ namespace RentCars.Models;
 public class Motorcycle : Vehicle
 {
   public double SeatHeight { get; set; }
-  public BrakeType FrontBreak { get; set; }
+  public BrakeType FrontBrake { get; set; }
   public BrakeType RearBrake { get; set; }
 }
